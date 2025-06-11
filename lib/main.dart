0586bfe0d9d63047b4ec69bc84286afe64a68889
cshyeon32 <139736071+cshyeon32@users.noreply.github.com>
2025-06-11 15:54:23 +0900

    ProfileScreen
    이메일 뜸

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:random_eat/screens/auth/login_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:random_eat/screens/profile/profile_screen.dart';
 import 'models/food_item.dart';
 import 'package:random_eat/services/seed_data.dart';
 import 'package:random_eat/screens/list_screen.dart';
@@ -17,16 +18,6 @@ void main() async {
   runApp(const MyApp());
 }
 
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       home: LoginScreen(),
-//     );
-//   }
-// }
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -41,22 +32,9 @@ class MyApp extends StatelessWidget {
         '/home': (context) => const HomeScreen(),
         '/list': (context) => const ListScreen(),
         '/add': (context) => const AddScreen(),
+        '/profile': (context) => const ProfileScreen(),
         // 필요한 다른 라우트도 추가 가능
       },
     );
   }
 }
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'RandomEat',
-//       theme: ThemeData(primarySwatch: Colors.blue),
-//       initialRoute: '/',
-//       routes: {
-//         '/': (context) => const HomeScreen(),
-//         '/list': (context) => const AddScreen(), // 여기 추가!
-//       },
-//     );
-//   }
-// }
